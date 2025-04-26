a1e4e2c7cbe1f76df8bd8fe8313df7bc686d1a55
Pankaj Singh <pankajsingh13042000@gmail.com>
2025-04-27 01:56:10 +0530

    Solved the tab change issue in MainActivity

--- app/src/main/java/com/example/aiassistant/presentation/MainActivity.kt
+++ app/src/main/java/com/example/aiassistant/presentation/MainActivity.kt
@@ -5,18 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.fragment.findNavController
 import com.example.aiassistant.R
 import com.example.aiassistant.databinding.ActivityMainBinding
+import com.example.aiassistant.presentation.imagechat.ImageChatFragment
+import com.example.aiassistant.presentation.voicechat.VoiceChatFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private var currentTabId = R.id.voiceChatFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -24,15 +29,41 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val navController = supportFragmentManager
-            .findFragmentById(R.id.nav_host_container)
-            ?.findNavController()
+        if (savedInstanceState == null) {
+            switchToFragment(R.id.voiceChatFragment)
+        }
 
-        navController?.let { nav ->
-            binding.bottomNav.setOnItemSelectedListener { item ->
-                nav.navigate(item.itemId)
-                true
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            if (item.itemId != currentTabId) {
+                switchToFragment(item.itemId)
             }
+            true
+        }
+    }
+
+    private fun switchToFragment(itemId: Int) {
+        val fragment = when (itemId) {
+            R.id.voiceChatFragment -> VoiceChatFragment()
+            R.id.imageChatFragment -> ImageChatFragment()
+            else -> null
+        }
+
+        fragment?.let {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_container, it)
+                .commitAllowingStateLoss()
+            currentTabId = itemId
+        }
+    }
+
+    override fun onBackPressed() {
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container)
+
+        if (navHostFragment?.childFragmentManager?.backStackEntryCount == 0) {
+            finishAfterTransition()
+        } else {
+            super.onBackPressed()
         }
     }
 }
